5d03cb0a912ed2ef90f9d624e497dfd895c63482
On <5586237+wm1045266943@user.noreply.gitee.com>
2021-03-04 17:37:55 +0800

    musicNew布局调整

--- src/network/request.js
+++ src/network/request.js
@@ -8,16 +8,16 @@ export function request(config) {
     timeout: 5000
   })
 
-  // // axios的拦截器
-  // // 请求拦截
-  // instance.interceptors.request.use(config => {
-  //   return config
-  // })
-  //
-  // // 响应拦截
-  // instance.interceptors.response.use(res => {
-  //   return res.data
-  // })
+  // axios的拦截器
+  // 请求拦截
+  instance.interceptors.request.use(config => {
+    return config
+  })
+
+  // 响应拦截
+  instance.interceptors.response.use(res => {
+    return res.data
+  })
 
   // 3.发送真正的网络请求
   return instance(config)
