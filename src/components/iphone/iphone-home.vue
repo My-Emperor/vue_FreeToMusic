aadb5f3013172c76b12cdcc51f4b508f0a1998f1
GradualJin <1045266943@qq.com>
2022-08-17 16:49:29 +0800

    iphone-component dev:/sheet

--- src/components/iphone/iphone-home.vue
+++ src/components/iphone/iphone-home.vue
@@ -162,7 +162,14 @@ export default {
   padding-bottom: constant(safe-area-inset-bottom); /* 兼容 iOS<11.2 */
   padding-bottom: env(safe-area-inset-bottom); /* 兼容iOS>= 11.2 */
   background-color: #fff;
+  :deep .var-loading__content{
+    display: flex;
+    flex-direction: column;
+    height: 100%;
+  }
   .loading-mask{
+    display: flex;
+    flex-direction: column;
     height: 100%;
     overflow: scroll;
   }
--- src/components/iphone/iphone-view/iphone-sheet.vue
+++ src/components/iphone/iphone-view/iphone-sheet.vue
@@ -1,34 +1,171 @@
 <template>
-  <div class="flex1">
+  <div class="sheet-box flex1">
+    <!--  两列布局 左侧选择 右侧歌单-->
+    <div class="tab-left">
+      <var-tabs
+          class="tabs-example-vertical"
+          layout-direction="vertical"
+          active-color="#00C48F"
+          v-model:active="currentActive"
+      >
+        <div v-for="(item1,index1) in sheetObj.catList.categories" :key="index1">
+          <div class="tab-title">{{ item1 }}</div>
+          <var-tab
+              @click="sheetObj.changeCategoriesList(item2.name)"
+              v-for="(item2,index2) in sheetObj.getCategoriesList(index1)"
+              :key="index2"
+              class="var-tab-item">
+            {{ item2.name }}
+          </var-tab>
+        </div>
 
+      </var-tabs>
+    </div>
+    <div @scroll="sheetObj.scrollEvent" class="scroll-right">
+      <song-sheet-box class="sheet-item-box"
+                      v-for="(item,index) in sheetObj.sheetList" :key="index"
+                      :imgUrl="item.coverImgUrl"
+                      :text="item.name">
+      </song-sheet-box>
+    </div>
   </div>
 </template>
-
 <script>
 
 import {SheetApi} from "@/api-modules/sheet"
-import {reactive} from "vue"
+import {ref, reactive} from "vue"
+import songSheetBox from "@/components/iphone/iphone-component/song-sheet-box";
 const sheetApi = new SheetApi();
 export default {
   name: "iphone-sheet",
-  setup(){
+  components: {
+    songSheetBox
+  },
+  setup() {
+    let loading =  ref(false);
+    let currentActive = ref(0);
     let sheetObj = reactive({
-      sheetList:[],
+      sheetList: [],
+      catList: [],
+      more: true,
+      params: {
+        order: "hot",//接口默认
+        cat: "华语",//指定默认:华语 接口默认:全部
+        limit: "10",
+        offset: "0"
+      },
+      // hotList: [],
+
+      //过滤category
+      getCategoriesList(catIndex) {
+        let list = sheetObj.catList.sub.filter(item => {
+          if (item.category == catIndex) {
+            return item.name;
+          }
+        })
+
+        return list;
+      },
+
+      //修改类型请求数据
+      changeCategoriesList(name){
+        sheetObj.params.cat = name;
+        sheetObj.params.offset = 0;
+        getPlayList(sheetObj);
+      },
+
+      //上拉滚动监听
+      scrollEvent(e) {
+        const { scrollHeight, scrollTop, clientHeight } = e.target;
+        if (scrollHeight - scrollTop <= clientHeight) {
+          console.log("触底")
+          console.log(loading.value)
+          if (!loading.value) {
+            loading.value = true;
+            // 触底
+            sheetObj.params.offset++;
+            getPlayList(sheetObj);
+          }
+        }
+      },
     })
-    sheetApi.getCatList().then(res => {
-      console.log(res)
-    })
-    sheetApi.getHotList().then(res => {
-      console.log(res)
-    })
+
+    // sheetApi.getHotList().then(res => {
+    //   console.log(res)
+    // })
+    function getPlayList(sheetObj){
+      sheetApi.getPlayList(sheetObj.params.order, sheetObj.params.cat, sheetObj.params.limit, sheetObj.params.offset).then(res => {
+        sheetObj.more = res.more;
+        sheetObj.sheetList = sheetObj.params.offset == 0 ? res.playlists : [...sheetObj.sheetList, ...res.playlists];
+        loading.value = false;
+      })
+    }
+    function getCatList(){
+      sheetApi.getCatList().then(res => {
+        sheetObj.catList = res;
+      })
+    }
+
+    //请求数据
+    getCatList();
+    getPlayList(sheetObj);
+
     return {
-      sheetObj
+      sheetObj,
+      currentActive
     }
 
   },
 }
 </script>
 
-<style scoped>
+<style lang="scss" scoped>
+.sheet-box {
+  display: flex;
+  height: 100%;
+  width: 375px;
+  flex: 1;
+
+  .tab-left {
+    display: flex;
+    flex-direction: column;
+    flex-shrink: 0;
+    border-right: 2px solid #ccc;
+
+    .tabs-example-vertical {
+      display: flex;
+      flex-direction: column;
+      flex: 1;
+      height: 100%;
+
+      .tab-title {
+        padding: 4px 0;
+        font-size: 14px;
+        background-color: #009d72;
+        //background-image: linear-gradient(to right,#fff,#00C48F );
+        color: #fff;
+        text-align: center;
+      }
+
+      .var-tab-item {
+        width: 100px;
+        height: 50px;
+        flex-shrink: 0;
+      }
+    }
+  }
+
+  .scroll-right {
+    display: flex;
+    flex-wrap: wrap;
+    flex: 1;
+    height: 100%;
+    overflow: scroll;
+    .sheet-item-box{
+      margin: 20px;
+      flex-shrink: 0;
+    }
+  }
+}
 
 </style>
